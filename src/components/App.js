911e0ab3d265bfd7e2812fb2573e6236d492f690
abhishek-ks1719 <114828154+abhishek-ks1719@users.noreply.github.com>
2023-05-17 19:45:13 +0530

    sol

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [click,setClick] = useState(0);
+  const handleClick = ()=>{
+        setClick(state=>state+1);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Button clicked {click} times</p>
+        <button onClick={handleClick}>Click me</button>
     </div>
   )
 }
